f2136b6655b4baa027369a395c4da47bbcabae3a
evolshenanigans <gjgutierrez91@gmail.com>
2023-09-06 00:13:51 -0700

    modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,21 +10,20 @@ import Why from "../app/Components/Why";
 export default function Home() {
   const [coins, setCoins] = useState([]);
 
-useEffect(() => {
-  const fetchData = async () => {
-    try {
-      const response = await fetch(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en"
-      );
-      const data = await response.json();
-      setCoins(data);
-    } catch (error) {
-      console.error("An error occurred while fetching data:", error);
-    }
-  };
-
-  fetchData();
-}, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en"
+        );
+        const data = await response.json();
+        setCoins(data);
+      } catch (error) {
+        console.error("There was a problem fetching the data:", error);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <main>
       <div className="relative">
