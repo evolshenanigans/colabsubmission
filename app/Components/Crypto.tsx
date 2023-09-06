2231102a37323a6838c9729031a925f2a93172e8
evolshenanigans <gjgutierrez91@gmail.com>
2023-09-06 00:06:02 -0700

    modified:   app/Components/Crypto.tsx

--- app/Components/Crypto.tsx
+++ app/Components/Crypto.tsx
@@ -1,4 +1,7 @@
+"use client"
+
 import Coins from "./Coins";
+import { useEffect, useState } from "react";
 
 interface Coin {
   id: string;
@@ -18,14 +21,33 @@ interface CryptoProps {
   coins: Coin[];
 }
 
-export default function Crypto({ coins }: CryptoProps) {
+export default function Crypto({ coins: initialCoins }: CryptoProps) {
+  const [coins, setCoins] = useState(initialCoins);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en"
+        );
+        const data = await response.json();
+        setCoins(data);
+      } catch (error) {
+        console.error("An error occurred while fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // Existing logic for rendering coins
   const topTenCoins = coins.slice(0, 10);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
+    <>
       {topTenCoins.map((coin) => (
         <Coins {...coin} key={coin.id} />
       ))}
-    </div>
+    </>
   );
 }
