aa6b6d6b994fc457b1b06a1a7003be06971707ff
evolshenanigans <gjgutierrez91@gmail.com>
2023-09-06 10:11:41 -0700

    modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,22 +8,22 @@ import React, { useEffect, useState } from "react";
 import Why from "../app/Components/Why";
 
 export default function Home() {
-  // const [coins, setCoins] = useState([]);
+  const [coins, setCoins] = useState([]);
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await fetch(
-  //         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en"
-  //       );
-  //       const data = await response.json();
-  //       setCoins(data);
-  //     } catch (error) {
-  //       console.error("There was a problem fetching the data:", error);
-  //     }
-  //   };
-  //   fetchData();
-  // }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en"
+        );
+        const data = await response.json();
+        setCoins(data);
+      } catch (error) {
+        console.error("There was a problem fetching the data:", error);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <main>
       <div className="relative">
@@ -36,7 +36,7 @@ export default function Home() {
         <Projects />
       </div>
       <Why />
-      {/* <Crypto coins={coins} /> */}
+      <Crypto coins={coins} />
       <div className="relative">
         <div className="gradient-04 z-0" />
       </div>
